216e03f4ff98c9f0d10f5826e92850ffd38e1e3d
Breno Rodrigues <brerodrigues@users.noreply.github.com>
2019-01-19 15:28:44 -0500

    separa por funcoes e implementa insercao de ranking de times

--- ctftimebr/main.py
+++ ctftimebr/main.py
@@ -5,12 +5,13 @@ import ctftimeapi
 import config
 from models.dbhelper import DbHelper
 
-def main():
+def update_ctf_events():
     api_url = config.API_URL
+    ctf_time_url = config.CTF_TIME_URL
 
     db = DbHelper('sqlite.db')
 
-    ctftime = ctftimeapi.CtftimeApi(api_url)
+    ctftime = ctftimeapi.CtftimeApi(api_url, ctf_time_url)
     # get all events from present to end of 2019
     ctf_events = ctftime.get_events_by_time('10000', str(int(time.time())), '1577762809')
     
@@ -23,7 +24,29 @@ def main():
         print ('Url: ' + ctf_event.url)
         print ('Duracao ' + ctf_event.duration)
         db.insert_ctf_event(ctf_event)
-        print ('-------------------------------------------------')
+        print ('-------------------------------------------------') 
+
+def update_ranking_teams():
+    api_url = config.API_URL
+    ctf_time_url = config.CTF_TIME_URL
+
+    db = DbHelper('sqlite.db')
+
+    ctftime = ctftimeapi.CtftimeApi(api_url, ctf_time_url)
+    # get the first 10 ctf br teams
+    ctf_br_teams = ctftime.get_br_teams(10)
+    db.connect_db()
+    db.drop_table_teams() # drop table because i'm too lazy to validade the duplicates
+    db.create_table_teams()
+    for ctf_team in ctf_br_teams:
+        print (ctf_team.name + '-' + str(ctf_team.position) + '-' + str(ctf_team.points))
+        db.insert_ctf_team_ranking(ctf_team)
+
+def main():
+    update_ctf_events()
+    update_ranking_teams()
+
+
 
 
 main()
